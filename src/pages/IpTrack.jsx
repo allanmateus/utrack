42baeebc248f5dcfd2707bfe6cb4de395fa346d8
Allan Mateus <72580107+allanmateus@users.noreply.github.com>
2023-08-01 10:01:51 -0300

    Add files via upload

--- src/pages/IpTrack.jsx
+++ src/pages/IpTrack.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react';
 import style from './IpTrack.module.css'
+import { useQuery } from 'react-query';
 
 
 function IpTrack() {   
-  const [ip, setIp] = useState();
+  const [ip, setIp] = useState('');
+  const [info, setInfo] = useState('');
 
-  function onSubmit(e){
-    e.prevent.default();
-    setIp(e.value);
-    console.log(e);
+  function handleSubmit(e){
+    setIp(e.target.value);
+    console.log(ip);
     const url = `https://ipgeolocation.abstractapi.com/v1/?api_key=91110d20bf094f3e93eff5156d5ab722&ip_address=${ip}`;
-    
-    fetch(url)
+
+
+    const {data} = useQuery("ips", () =>{
+      fetch(url)
       .then(res => res.json())
-      .then(console.log(res))
       .catch(error => console.log(error))
+    }); 
   }
 
 return (
   <>
     <h1 className = {style.h1}>IpTrack</h1>
-    <form className={style.form}>
-      <div className='form-group'>
+    <form className={style.form} onSubmit={handleSubmit}>
         <label htmlFor="inputIp">IP</label>
-        <input type="text" placeholder='Digite o IP'/>
-      </div>
-      <div className='form-group'>
-        <button onClick={onSubmit(e)}>Pesquisar</button>
-      </div>
+        <input type="text" value={ip} placeholder='Digite o IP'/>
+        <button type="submit">Pesquisar</button>
     </form>
   </>
 )
